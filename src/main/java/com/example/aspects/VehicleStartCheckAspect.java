cc4d76ca278038b822269ad35243187784c31452
Sanyam Pandey <sanyam071104@gmail.com>
2026-01-29 23:53:35 +0530

    Initialised the @Before aspect for the methods

---
+++ src/main/java/com/example/aspects/VehicleStartCheckAspect.java
@@ -0,0 +1,10 @@
+package com.example.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class VehicleStartCheckAspect{
+
+}
